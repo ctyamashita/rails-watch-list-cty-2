e61626114a8ddfe16666e45e9dbbca9924264b05
Celso Yamashita <ctyamashita@hotmail.com>
2022-08-12 00:00:45 +0900

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[create new]
+    resources :reviews, only: %i[create new]
+  end
+
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
