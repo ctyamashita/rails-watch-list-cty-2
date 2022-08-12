21a9a0e559b43fc9f5f69a5e96cad91ce65cb4bc
Celso Yamashita <ctyamashita@hotmail.com>
2022-08-12 13:44:30 +0900

    custom validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,10 @@ class List < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
+
+  validate :dayo
+
+  def dayo
+    errors.add(:name, 'error message!') if !name.include?('dayo')
+  end
 end
